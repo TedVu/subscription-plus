c108797fbdd3a8026cdbd8421537ecc3d832ac88
TedEngineer <tedvu184@gmail.com>
2023-12-13 21:59:01 +1100

    implement onhover navigation drawer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,24 @@
-<script lang="ts" setup>
-import { ref } from 'vue';
-const drawer = ref(false);
-</script>
-
+<script lang="ts" setup></script>
 <template>
   <v-app>
     <v-app-bar color="info" density="comfortable">
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       Subscription plus
     </v-app-bar>
-    <v-navigation-drawer v-if="drawer">
+    <v-navigation-drawer expand-on-hover rail>
       <v-list>
-        <v-list-item title="Profile"></v-list-item>
-        <v-list-item title="Home"></v-list-item>
-        <v-list-item title="About"></v-list-item>
+        <v-list-item title="Ted Vu" subtitle="tedvu184@gmail.com"></v-list-item>
+        <v-divider></v-divider>
+        <v-list-item
+          prepend-icon="mdi-account-multiple"
+          link
+          title="Profile"
+        ></v-list-item>
+        <v-list-item prepend-icon="mdi-home" link title="Home"></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-information"
+          link
+          title="About"
+        ></v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-main><slot /></v-main>
