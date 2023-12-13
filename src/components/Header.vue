0d870f1ef0d1f0c465f4477f1401e4ab160577e8
TedEngineer <tedvu184@gmail.com>
2023-12-13 21:53:47 +1100

    adding navigation drawer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,6 +9,13 @@ const drawer = ref(false);
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       Subscription plus
     </v-app-bar>
+    <v-navigation-drawer v-if="drawer">
+      <v-list>
+        <v-list-item title="Profile"></v-list-item>
+        <v-list-item title="Home"></v-list-item>
+        <v-list-item title="About"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main><slot /></v-main>
   </v-app>
 </template>
