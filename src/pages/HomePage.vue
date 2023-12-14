4477007073a6c7a0a7d999ccce43d6bfb47e8aae
TedEngineer <tedvu184@gmail.com>
2023-12-14 23:16:43 +1100

    exploring layouts

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,72 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>This is the home page</div>
+  <div class="icons-layout">
+    <v-card class="mx-auto icons-item" max-width="400">
+      <v-img
+        class="align-end text-white"
+        height="200"
+        src="https://tedvublogimages.s3.us-west-1.amazonaws.com/profilepics.jpg"
+        cover
+      >
+        <v-card-title>Top 10 Australian beaches</v-card-title>
+      </v-img>
+
+      <v-card-subtitle class="pt-4"> Number 10 </v-card-subtitle>
+
+      <v-card-text>
+        <div>Whitehaven Beach</div>
+
+        <div>Whitsunday Island, Whitsunday Islands</div>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="orange"> Share </v-btn>
+
+        <v-btn color="orange"> Explore </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card class="mx-auto icons-item" max-width="400">
+      <v-img
+        class="align-end text-white"
+        height="200"
+        src="https://tedvublogimages.s3.us-west-1.amazonaws.com/profilepics.jpg"
+        cover
+      >
+        <v-card-title>Top 10 Australian beaches</v-card-title>
+      </v-img>
+
+      <v-card-subtitle class="pt-4"> Number 10 </v-card-subtitle>
+
+      <v-card-text>
+        <div>Whitehaven Beach</div>
+
+        <div>Whitsunday Island, Whitsunday Islands</div>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="orange"> Share </v-btn>
+
+        <v-btn color="orange"> Explore </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
+
+<style>
+.icons-layout {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fill, 150px);
+  justify-content: space-around;
+  vertical-align: middle;
+
+  .icons-item {
+    display: flex;
+    padding-top: 20px;
+    align-items: center;
+    flex-direction: column;
+    height: 330px;
+  }
+}
+</style>
