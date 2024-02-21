10d4737cc36edd603ef0b8979cef8b16591d8427
TedEngineer <tedvu184@gmail.com>
2024-02-21 19:44:45 +1100

    add dialog to implement update action

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -36,12 +36,35 @@ onMounted(async () => {
           <v-card-actions>
             Subscription Date: {{ item.date }}
             <v-spacer />
-            <v-btn
-              size="small"
-              color="surface-variant"
-              variant="text"
-              icon="mdi-update"
-            ></v-btn>
+            <v-dialog width="500">
+              <template #activator="{ props }">
+                <v-btn
+                  v-bind="props"
+                  size="small"
+                  color="surface-variant"
+                  variant="text"
+                  icon="mdi-update"
+                ></v-btn>
+              </template>
+              <template #default="{ isActive }">
+                <v-card title="Dialog">
+                  <v-card-text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                      text="Close Dialog"
+                      @click="isActive.value = false"
+                    ></v-btn>
+                  </v-card-actions>
+                </v-card>
+              </template>
+            </v-dialog>
           </v-card-actions>
         </v-card>
       </v-col>
