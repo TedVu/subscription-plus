d6b3e3daacfad0cea439224c281310de450f8007
TedEngineer <tedvu184@gmail.com>
2024-03-10 15:44:12 +1100

    configure build - remove dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import del from 'rollup-plugin-delete';
 
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    rollupOptions: {
+      plugins: [del({ targets: 'dist/*' })],
+    },
+  },
+});
