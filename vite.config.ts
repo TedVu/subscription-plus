7016bf472bb53c6447a6f3e9d58c00fb628be7eb
TedEngineer <tedvu184@gmail.com>
2024-03-29 22:18:09 +1100

    add manual chunk

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,17 @@ export default defineConfig({
   build: {
     rollupOptions: {
       plugins: [del({ targets: 'dist/*' })],
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
     },
   },
 });
