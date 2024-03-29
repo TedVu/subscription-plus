079ce5922e519e006b45d867a46a76f0a82b3125
TedEngineer <tedvu184@gmail.com>
2024-03-29 22:29:15 +1100

    add rollup plugin visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import del from 'rollup-plugin-delete';
+import { splitVendorChunkPlugin } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), splitVendorChunkPlugin(), visualizer()],
   build: {
     rollupOptions: {
       plugins: [del({ targets: 'dist/*' })],
