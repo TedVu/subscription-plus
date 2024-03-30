a3614a391c362d8d4938a8df39bada2873eac24c
TedEngineer <tedvu184@gmail.com>
2024-03-30 16:55:32 +1100

    boostrapping login page with basic controls

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,5 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+const name = ref('');
+const password = ref('');
+const loading = ref(false);
+const snackbar = ref(false);
+const snackbarMsg = ref('');
+const snackbarColor = ref('');
+const submit = async () => {};
+</script>
 
 <template>
-  <div>LOGIN</div>
+  <v-sheet width="300" class="mx-auto">
+    <v-form validate-on="blur" @submit.prevent="submit">
+      <v-text-field v-model="name" label="Username" />
+      <v-text-field v-model="password" label="Password" />
+
+      <v-btn
+        :loading="loading"
+        type="submit"
+        block
+        class="mt-2"
+        color="success"
+        variant="elevated"
+        >Login</v-btn
+      >
+      <v-btn
+        :loading="loading"
+        type="submit"
+        block
+        class="mt-2"
+        color="primary"
+        variant="elevated"
+        >Register</v-btn
+      >
+    </v-form>
+    <v-snackbar v-model="snackbar" :color="snackbarColor">
+      {{ snackbarMsg }}
+    </v-snackbar>
+  </v-sheet>
 </template>
