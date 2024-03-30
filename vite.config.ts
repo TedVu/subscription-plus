f9d38e903943b8fbc1e1604f27bc6f87821e14bf
TedEngineer <tedvu184@gmail.com>
2024-03-30 18:49:33 +1100

    tree shaking for vuetify

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import del from 'rollup-plugin-delete';
 import { splitVendorChunkPlugin } from 'vite';
+import vuetify from 'vite-plugin-vuetify';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [vue(), splitVendorChunkPlugin(), visualizer()],
+  plugins: [vue(), splitVendorChunkPlugin(), vuetify(), visualizer()],
   build: {
     rollupOptions: {
       plugins: [del({ targets: 'dist/*' })],
