38afcd95d06eb0448c565b9fa711663217b0e8eb
TedEngineer <tedvu184@gmail.com>
2024-03-30 16:45:06 +1100

    boostrapping login page

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import { About, HomePage, Profile, Add } from '../pages';
 import * as VueRouter from 'vue-router';
+import Login from '../pages/Login/Login.vue';
 
 const routes = [
   { path: '/about', component: About },
   { path: '/', component: HomePage },
   { path: '/profile', component: Profile },
+  { path: '/login', component: Login },
   { path: '/add', component: Add },
 ];
 
