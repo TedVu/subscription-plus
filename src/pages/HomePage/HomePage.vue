9141b9e7d27d29753dbd7d2f938f91a4bcb98f6f
TedEngineer <tedvu184@gmail.com>
2024-04-01 17:50:14 +1100

    remove redundant computed

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, Ref, watch } from 'vue';
+import { ref, Ref, watch } from 'vue';
 import { storeToRefs } from 'pinia';
 import { computedAsync } from '@vueuse/core';
 import draggable from 'vuedraggable';
