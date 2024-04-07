332baf8df8dee43c56fdb30ba7bef01cfb96a5e5
TedEngineer <tedvu184@gmail.com>
2024-04-07 17:11:41 +1000

    remove redundant import

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, isSupported } from "firebase/messaging";
+import { getMessaging, getToken } from "firebase/messaging";
 import { getAuth } from "firebase/auth";
 
 import {
