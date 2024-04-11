616c5ad238939a535226e9abb66685ecc628cbe1
TedEngineer <tedvu184@gmail.com>
2024-04-11 22:40:40 +1000

    change naming to not found

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,6 +3,6 @@ import HomePage from "./HomePage/HomePage.vue";
 import Profile from "./Profile/Profile.vue";
 import Add from "./Add/Add.vue";
 import Reminder from "./Reminder/Reminder.vue";
-import NotFound from "./404/404.vue";
+import NotFound from "./NotFound/NotFound.vue";
 
 export { About, Add, HomePage, Profile, Reminder, NotFound };
