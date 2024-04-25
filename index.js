0b208b610f5f2afc3194dbcb30ab6a9ce5bd4830
TedEngineer <tedvu184@gmail.com>
2024-04-26 08:42:01 +1000

    cleanup firebase fcm

--- index.js
+++
@@ -1,8 +0,0 @@
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.onBackgroundMessage` handler.
-messaging.onMessage((payload) => {
-  console.log("Message received. ", payload);
-  // ...
-});
