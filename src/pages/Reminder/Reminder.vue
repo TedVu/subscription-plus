1ea3a09b0643f9bc1c41aaf886d6389684144991
TedEngineer <tedvu184@gmail.com>
2024-06-02 22:19:53 +1000

    start implementing the messaging

--- src/pages/Reminder/Reminder.vue
+++ src/pages/Reminder/Reminder.vue
@@ -3,12 +3,12 @@
 </template>
 
 <script lang="ts" setup>
-// import { _getToken } from "../../firebase";
+import { _getToken } from "../../firebase";
 const requestPermission = () => {
   Notification.requestPermission().then((permission) => {
     if (permission === "granted") {
       console.log("Notification permission granted.");
-      // _getToken();
+      _getToken();
     } else {
       console.log("Permission denied");
     }
