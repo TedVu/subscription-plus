7d4ebebed3b4ab40d03dd46ce098c83c956db53e
TedEngineer <tedvu184@gmail.com>
2024-08-08 23:18:06 +1000

    initial boostrap of profile page

--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -8,4 +8,11 @@ const { userRef } = useAuthentication();
   <div class="text-h5">
     Welcome to Subscription Plus Application, {{ userRef?.displayName }}!
   </div>
+  <v-card class="mx-auto mt-8" max-width="434" rounded="0">
+    <v-avatar color="grey" rounded="0" size="150">
+      <v-img :src="userRef?.photoURL ?? ''" cover></v-img>
+    </v-avatar>
+
+    <div>Number of subscription: 5</div>
+  </v-card>
 </template>
