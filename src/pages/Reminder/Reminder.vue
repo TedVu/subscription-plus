6b04ec58e1a74dd9e6d125e30f8ae60f7cda4a89
Ted Vu <tedvu184@gmail.com>
2025-03-25 09:24:07 +1100

    boostrap reminder page

--- src/pages/Reminder/Reminder.vue
+++ src/pages/Reminder/Reminder.vue
@@ -1,3 +1,21 @@
-<template></template>
+<template>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="12" sm="4">
+        <div class="ma-4">
+          <div class="text-subtitle-2">Cover</div>
+          <v-img
+            :aspect-ratio="1"
+            class="bg-white"
+            src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+            width="300"
+            cover
+          ></v-img>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="8"> DETAILS</v-col>
+    </v-row>
+  </v-container>
+</template>
 
 <script lang="ts" setup></script>
